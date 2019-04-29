25c51ee85da642753c4813daf78660500b25d255
Fedorov113 <fedorov.de@gmail.com>
2019-04-30 00:51:13 +0300

    Update metawrap_classify_bins.py

--- results/metawrap_classify_bins/metawrap_classify_bins.py
+++ results/metawrap_classify_bins/metawrap_classify_bins.py
@@ -58,11 +58,29 @@ rule taxator_contigs:
         sort -k1,1 {output.predictions} | binner -n classification -i genus:0.6 > {output.binned_predictions}; \n
             cat {output.binned_predictions} | taxknife -f 2 --mode annotate -s path | grep -v "Could not" | cut -f1,2 > {output.contig_taxonomy};''')
 
+rule extract_predictions_for_bin:
+    input: 
+        contig_taxonomy =  os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000__no_hum_centr.contig_taxonomy.tab'),
+        names = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/{collection}/bin_by_bin/{binn}/{binn}-contigs.names')
+    output: 
+        tax = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/{collection}/bin_by_bin/{binn}/{binn}-contigs.contig_taxonomy.tab')
+
+    # shell: ('''cat {input.contig_taxonomy} | grep "^$line" {input.contig_taxonomy} > {output.tax}''')
+    run: 
+        names = []
+        with open(input.names, 'r') as n:
+            names = [nn.strip() for nn in n.readlines()]
+        for name in names:
+            command_wc = 'grep "^{name}" {input} >> {output} || true'
+            command = command_wc.format(name=name, output=output.tax, input=input.contig_taxonomy)
+            shell(command)
+
 rule classify_bin:
     input: 
-        tax = '{prefix}/{df}/anvio/merged_profiles/bwa__{bwa_params}___assembly___mh__{params}___{dfs}___{samples}___{preprocs}/MERGED/SUMMARY/bin_by_bin/{bin}/{bin}-contigs.contig_taxonomy.tab',
-        bin_contigs = '{prefix}/{df}/anvio/merged_profiles/bwa__{bwa_params}___assembly___mh__{params}___{dfs}___{samples}___{preprocs}/MERGED/SUMMARY/bin_by_bin/{bin}/{bin}-contigs.fa'
-    output:'{prefix}/{df}/anvio/merged_profiles/bwa__{bwa_params}___assembly___mh__{params}___{dfs}___{samples}___{preprocs}/MERGED/SUMMARY/bin_by_bin/{bin}/{bin}-bin_taxonomy.tab'
+        tax = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/{collection}/bin_by_bin/{binn}/{binn}-contigs.contig_taxonomy.tab'),
+        bin_contigs = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/{collection}/bin_by_bin/{binn}/{binn}-contigs.fa')
+    output:
+        os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/{collection}/bin_by_bin/{binn}/{binn}-bin_taxonomy.tab')
     conda: 'env.yaml'
     threads: 1
     shell: ('''python2.7 {classify_bins} {input.tax} {input.bin_contigs} > {output}''')
