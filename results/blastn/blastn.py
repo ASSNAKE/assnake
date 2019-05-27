8912d6603b55b09a53ecb1dfbe62326dd93d8633
Fedorov113 <fedorov.de@gmail.com>
2019-05-28 01:21:40 +0300

    cat

--- results/blastn/blastn.py
+++ results/blastn/blastn.py
@@ -14,7 +14,17 @@ rule classify_contigs:
     input: os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000__no_hum_centr.fa')
     output: os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000__no_hum_centr.megablast_out.raw.fa')
     conda: 'env.yaml'
-    threads: 40
+    threads: 12
+    shell: ('''blastn -task megablast -num_threads {threads} \
+            -db {BLASTDB}/nt \
+            -outfmt '6 qseqid qstart qend qlen sseqid staxids sstart send bitscore evalue nident length' \
+            -query {input} > {output}''')
+
+rule classify_contigs_2:
+    input: os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000.fa')
+    output: os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000.megablast_out.raw.fa')
+    conda: 'env.yaml'
+    threads: 12
     shell: ('''blastn -task megablast -num_threads {threads} \
             -db {BLASTDB}/nt \
             -outfmt '6 qseqid qstart qend qlen sseqid staxids sstart send bitscore evalue nident length' \
--- results/cat_bat/cat_bat.py
+++ results/cat_bat/cat_bat.py
@@ -16,6 +16,29 @@ rule cat_bat_CAT:
         CAT bins -b {params.bin_folder} -d {cat_bat_db} -t {cat_bat_taxa} --bin_suffix .fa --out_prefix {params.prefix} -n {threads}; \n
             touch {output.done}''')
 
+rule cat_bat_contigs:
+    input:
+        fa     = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000.fa')
+    output:
+        done   = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000_CAT/cat.done'),
+        classification = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000_CAT/out.CAT.contig2classification.txt')
+    params:
+        wd     = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000_CAT/'),
+        prefix = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000_CAT/out.CAT'),
+    threads: 24
+    conda: 'cat_bat_env.yaml'
+    shell: ('''mkdir -p {params.wd}; \n
+            CAT contigs -c {input.fa} -d {cat_bat_db} -t {cat_bat_taxa} --out_prefix {params.prefix} -n {threads}; \n
+            touch {output.done}''')
+
+rule add_names2:
+    input:
+        classification = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000_CAT/out.CAT.contig2classification.txt')
+    output:
+        names = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/final_contigs__1000_CAT/contig2classification_names_official.txt')
+    conda: 'cat_bat_env.yaml'
+    shell: ('''CAT add_names -i {input.classification} -o {output.names} -t {cat_bat_taxa} --only_official''')
+
 rule add_names:
     input:
         done           = os.path.join(fna_db_dir, 'assembly/mh__{params}/{dfs}/{samples}/{preprocs}/{collection}/cat_bins.done'),
