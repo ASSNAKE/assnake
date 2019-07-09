6da7cd6bff06519adc8c237099df7d58aa29fa63
Fedorov113 <fedorov.de@gmail.com>
2019-07-10 00:57:58 +0300

    subset

--- assnake.py
+++ assnake.py
@@ -68,7 +68,8 @@ def create_df(config, df, fs_prefix):
 @click.command()
 @click.option('--name','-n', prompt='Name of the dataset', help='Name of the dataset' )
 @click.option('--preproc','-p', help='Show samples for preprocessing', required=False)
-def df_info(name, preproc):
+@click.pass_obj
+def df_info(config, name, preproc):
     """View info for the specific dataset"""
     print(preproc)
 
@@ -78,6 +79,11 @@ def df_info(name, preproc):
     click.echo('Filesystem prefix: ' + df_info.get('fs_prefix', ''))
     click.echo('Full path: ' + os.path.join(df_info.get('fs_prefix', ''), name))
     click.echo('Description: ' + df_info.get('description', ''))
+    
+    mg_samples_loc = os.path.join(config['config']['assnake_db'], 'datasets', df_info['df'], 'mg_samples.tsv')
+
+    if os.path.isfile(mg_samples_loc):
+        click.echo(tabulate(pd.read_csv(mg_samples_loc, sep='\t'), headers='keys', tablefmt='fancy_grid'))
 
     reads_dir = os.path.join(df_info['fs_prefix'], df_info['df'], 'reads/*')
     preprocs = [p.split('/')[-1] for p in glob.glob(reads_dir)]
@@ -93,6 +99,7 @@ def df_info(name, preproc):
         preprocessing.update({p:samples})
 
     click.echo('\nTotal samples: ' + str(len(set(all_samples))) + '\n')
+
 
     for key, value in preprocessing.items():
         click.echo('Samples in ' + click.style(key, bold=True) + ': ' + str(len(value)))
@@ -111,10 +118,13 @@ def result():
     """Commands to work with results"""
     pass
 
+example_res = ['fastqc', 'count', 'metaphlan2']
+
 @click.command()
-@click.option('--df','-d', prompt='Name of the dataset', help='Name of the dataset' )
-@click.option('--preproc','-p', prompt='Preprocessing to use', help='Preprocessing to use' )
-@click.option('--result','-r', prompt='Result you want', help='Preprocessing to use' )
+@click.option('--df','-d', help='Name of the dataset' )
+@click.option('--preproc','-p', help='Preprocessing to use' )
+@click.option('--results','-r', prompt='Results you want', help='Results you want', default=','.join(example_res), show_default=True,
+              metavar='<results>', type=click.STRING )
 
 @click.option('--params', help='Parameters id to use', required=False, default = 'def')
 @click.option('--list-name', help='Name of sample set if required by rule')
@@ -123,50 +133,63 @@ def result():
 @click.option('--jobs','-j', help='Number of jobs', default=1)
 @click.option('--run/--no-run', default=False)
 @click.pass_obj
-def request(config, df, preproc, result, params, list_name,   threads, jobs, run):
+def request(config, df, preproc, results, params, list_name,   threads, jobs, run):
     """Request result for your samples.\nYou need to provide info defining sample set, or run it from {prefix}/{df}/reads/{preproc} directory"""
 
     ss = api.sample_set.SampleSet()
-    df = api.loaders.load_df_from_db(df)
-    print(df)
-    ss.add_samples(df['fs_prefix'], df['df'], preproc)
-    click.echo(tabulate(ss.samples_pd[['fs_name', 'reads', 'preproc']].sort_values('reads'), headers='keys', tablefmt='fancy_grid'))
+    if df is not None:
+        df = api.loaders.load_df_from_db(df)
+        print(df)
+        ss.add_samples(df['fs_prefix'], df['df'], preproc)
+        click.echo(tabulate(ss.samples_pd[['fs_name', 'reads', 'preproc']].sort_values('reads'), headers='keys', tablefmt='fancy_grid'))
 
     res_list = []
+    # split results by ',' and remove whitespace
+    results = [c.strip() for c in results.split(',')]
+    for result in results:
+        if result == 'multiqc':
+            ss.prepare_fastqc_list_multiqc('R1', preproc)
+            ss.prepare_fastqc_list_multiqc('R2', preproc)
+            multiqc_report_wc = config['wc_config']['multiqc_report']
 
-    if result == 'multiqc':
-        ss.prepare_fastqc_list_multiqc('R1', preproc)
-        ss.prepare_fastqc_list_multiqc('R2', preproc)
-        multiqc_report_wc = config['wc_config']['multiqc_report']
+            res_list = [
+                multiqc_report_wc.format(
+                    prefix = df['fs_prefix'],
+                    df = df['df'],
+                    sample_set = preproc,
+                    strand = strand,
+                )
+                for strand in ['R1', 'R2']
+            ]
 
-        res_list = [
-            multiqc_report_wc.format(
-                prefix = df['fs_prefix'],
-                df = df['df'],
-                sample_set = preproc,
-                strand = strand,
-            )
-            for strand in ['R1', 'R2']
-        ]
-
-    elif result=='mp2':
-        res_list = ss.get_locs_for_result(result)
-    elif result=='fastqc':
-        res_list = ss.get_locs_for_result(result)
-    elif result == 'dada2-filter-and-trim':
-        res_list = ss.get_locs_for_result(result, params=params)
-    elif result=='dada2-learn-errors':
-        # ss.prepare_dada2_sample_list()
-        if list_name is None:
-            list_name = click.prompt('Please enter name for your dada2 error profile')
-            print(list_name)
-        ss.prepare_dada2_sample_list(list_name)
-        res_list= [(config['wc_config']['dada2_err_wc'].format(
-                dada2_dir = config['config']['dada2_dir'],
-                sample_set = list_name,
-                strand = strand,
-                params=params
-            )) for strand in ['R1', 'R2']]
+        elif result=='mp2':
+            res_list = ss.get_locs_for_result(result)
+        elif result=='count':
+            res_list = ss.get_locs_for_result(result)
+        elif result=='fastqc':
+            res_list = ss.get_locs_for_result(result)
+        elif result == 'dada2-filter-and-trim':
+            res_list = ss.get_locs_for_result(result, params=params)
+        elif result=='dada2-learn-errors':
+            # ss.prepare_dada2_sample_list()
+            if list_name is None:
+                list_name = click.prompt('Please enter name for your dada2 error profile')
+                print(list_name)
+            if df is not None:
+                ss.prepare_dada2_sample_list(list_name)
+                res_list= [(config['wc_config']['dada2_err_wc'].format(
+                        dada2_dir = config['config']['dada2_dir'],
+                        sample_set = list_name,
+                        strand = strand,
+                        params=params
+                    )) for strand in ['R1', 'R2']]
+            else:
+                res_list= [(config['wc_config']['dada2_err_wc'].format(
+                        dada2_dir = config['config']['dada2_dir'],
+                        sample_set = list_name,
+                        strand = strand,
+                        params=params
+                    )) for strand in ['R1', 'R2']]
         
 
     
@@ -182,6 +205,7 @@ def request(config, df, preproc, result, params, list_name,   threads, jobs, run
         configfile=os.path.join(curr_dir, 'config.yml'),
         drmaa_log_dir = os.path.join(curr_dir, '../pipeline_dev/drmaa_log'),
         use_conda = True,
+        latency_wait = 120,
         conda_prefix = os.path.join(curr_dir, '../pipeline_dev/conda'),
         drmaa=' -V -S /bin/bash -pe make {threads}'.format(threads=threads),
         cores=jobs, nodes=jobs
