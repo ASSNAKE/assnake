10b7929bc33ddfffbfef452f7aeb2e7309ba3cc7
albigel <dtumandda@gmail.com>
2020-02-03 17:18:15 +0300

    delete snakemake

---
+++ tests/dataset_command/dataset_input_test.py
@@ -0,0 +1,12 @@
+import pytest
+import unittest.mock as mock
+import assnake.cli.commands.dataset_commands
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
