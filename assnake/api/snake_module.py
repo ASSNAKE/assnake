eca63679acdf94de87103c87089968ca017731c8
albigel <dtumandda@gmail.com>
2020-02-09 20:25:59 +0300

    change `assnake da

--- assnake/api/snake_module.py
+++ assnake/api/snake_module.py
@@ -3,11 +3,13 @@ class SnakeModule:
     install_dir = ''
     snakefiles = []
     invocation_commands = []
+    initialization_commands = []
     wc_configs = []
 
-    def __init__(self, name, install_dir, snakefiles, invocation_commands, wc_configs=[]):
+    def __init__(self, name, install_dir, snakefiles, invocation_commands, initialization_commands, wc_configs=[]):
         self.name = name
         self.install_dir = install_dir
         self.snakefiles = snakefiles
         self.invocation_commands = invocation_commands
+        self.initialization_commands = initialization_commands
         self.wc_configs = wc_configs
