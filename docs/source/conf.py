af4a13a94870362fe96415e7855bc085fba36d9d
albigel <dtumandda@gmail.com>
2020-03-02 17:28:42 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../../assnake'))
 project = 'assnake'
 copyright = '2020, Fedorov'
 author = 'Fedorov'
-
+master_doc = 'index'
 # The full version, including alpha/beta/rc tags
 release = 'soon'
 
